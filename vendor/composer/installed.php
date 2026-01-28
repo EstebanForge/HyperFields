a3990e8893ee1de9dcaab9754642a1bc6b81bf55
EstebanForge <esteban@attitude.cl>
2026-01-27 21:18:01 -0300

    composer update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,20 +1,20 @@
 <?php return array(
     'root' => array(
         'name' => 'estebanforge/hyperfields',
-        'pretty_version' => '1.0.5',
-        'version' => '1.0.5.0',
+        'pretty_version' => '1.0.8',
+        'version' => '1.0.8.0',
         'reference' => null,
-        'type' => 'wordpress-plugin',
+        'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
         'dev' => false,
     ),
     'versions' => array(
         'estebanforge/hyperfields' => array(
-            'pretty_version' => '1.0.5',
-            'version' => '1.0.5.0',
+            'pretty_version' => '1.0.8',
+            'version' => '1.0.8.0',
             'reference' => null,
-            'type' => 'wordpress-plugin',
+            'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
